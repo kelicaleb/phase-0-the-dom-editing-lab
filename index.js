0d195ab72624c030cf8dbb29d0034f4168d0e8d7
kelicaleb <kelicaleb7@gmail.com>
2022-08-31 05:23:20 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body').innerHTML;
+const header = document.querySelector('header').innerHTML;
+const paragraph = document.querySelector('paragraph').innerHTML;
+const strong = document.querySelector('strong').innerHTML;
+const atag = document.querySelector('atag').innerHTML;
+const em = document.querySelector('em').innerHTML;
+const table = document.querySelector('table').innerHTML;
+
