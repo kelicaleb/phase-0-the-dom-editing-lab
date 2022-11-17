0ef30380bd20cb1caf8ee8cf72cd37c11116aee6
kelicaleb <kelicaleb7@gmail.com>
2022-11-17 19:09:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const body = document.querySelector('body').innerHTML;
-const header = document.querySelector('header').innerHTML;
-const paragraph = document.querySelector('paragraph').innerHTML;
-const strong = document.querySelector('strong').innerHTML;
-const atag = document.querySelector('atag').innerHTML;
-const em = document.querySelector('em').innerHTML;
-const table = document.querySelector('table').innerHTML;
-
+var h1 = document.createElement('h1')
+h1.innerHTML = "My HTML adventure"
+document.body.append(h1)
